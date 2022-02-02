07c00a04ffb45f94763c2272bb1b01bbf4007d8b
SULTAN-MURAT <98175937+SULTAN-MURAT@users.noreply.github.com>
2022-02-02 17:36:59 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,126 @@
+const btnAll = document.querySelectorAll(".btn-item");
+const mealContainer = document.querySelector(".section-center");
+const template = document.querySelector("template");
+
+const menu = [
+  {
+    id: 1,
+    title: "Tteokbokki",
+    category: "Korea",
+    price: 10.99,
+    img: "https://twoplaidaprons.com/wp-content/uploads/2020/09/tteokbokki-top-down-view-of-tteokbokki-in-a-bowl-500x500.jpg",
+    desc: `Spicy rice cakes, serving with fish cake.`,
+  },
+  {
+    id: 2,
+    title: "Chicken Ramen",
+    category: "Japan",
+    price: 7.99,
+    img: "https://www.forkknifeswoon.com/wp-content/uploads/2014/10/simple-homemade-chicken-ramen-fork-knife-swoon-01.jpg",
+    desc: `Chicken noodle soup, serving with vegetables such as soy bean, green onion. In an optional you can ask for egg. `,
+  },
+  {
+    id: 3,
+    title: "Bibimbap",
+    category: "Korea",
+    price: 8.99,
+    img: "https://dwellbymichelle.com/wp-content/uploads/2020/05/DWELL-bibimbap.jpg",
+    desc: `Boiling vegetables, serving with special hot sauce`,
+  },
+  {
+    id: 4,
+    title: "Dan Dan Mian",
+    category: "China",
+    price: 5.99,
+    img: "https://www.savingdessert.com/wp-content/uploads/2019/02/Dan-Dan-Noodles-10.jpg",
+    desc: `Dan dan noodle, serving with green onion `,
+  },
+  {
+    id: 5,
+    title: "Yangzhou Fried Rice",
+    category: "China",
+    price: 12.99,
+    img: "https://salu-salo.com/wp-content/uploads/2013/02/Yangzhou-Fried-Rice1.jpg",
+    desc: `Yangzhou style fried rice, serving with bean and pickles `,
+  },
+  {
+    id: 6,
+    title: "Onigiri",
+    category: "Japan",
+    price: 9.99,
+    img: "https://www.manusmenu.com/wp-content/uploads/2017/08/Onigiri-3-1-of-1.jpg",
+    desc: `Rice Sandwich, serving with soy sauce`,
+  },
+  {
+    id: 7,
+    title: "Jajangmyeon",
+    category: "Korea",
+    price: 15.99,
+    img: "https://www.curiouscuisiniere.com/wp-content/uploads/2020/04/Jajangmyeon-Korean-Noodles-in-Black-Bean-Sauce5.1200H-720x540.jpg",
+    desc: `Black bean sauce noodle, serving with green onion `,
+  },
+  {
+    id: 8,
+    title: "Ma Yi Shang Shu",
+    category: "China",
+    price: 12.99,
+    img: "https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/F688C2F6-86EC-46C4-B9C7-A6BA01DF7437/Derivates/32E3E72A-F786-406D-AF7F-B30980A9AC6C.jpg",
+    desc: `Hot pepper sauce noodle, serving with soy bean and onion`,
+  },
+  {
+    id: 9,
+    title: "Doroyaki",
+    category: "Japan",
+    price: 3.99,
+    img: "https://www.justonecookbook.com/wp-content/uploads/2011/10/Dorayaki-New-500x400.jpg",
+    desc: `Red bean paste dessert, serving with honey.`,
+  },
+];
+let category = "all";
+let filteredMeals = [];
+
+function fillMealContainer() {
+  console.log(this.id);
+  category = this.id;
+  clearContainer(mealContainer);
+  filterMeals(category);
+}
+
+function clearContainer(container) {
+  container.innerHTML = "";
+}
+
+function filterMeals(category) {
+  if(category=="all"){
+    filteredMeals=menu.map(meal=>{
+      const mealCard = copyMealCard(meal);
+      mealContainer.appendChild(mealCard);
+    });
+  }
+  filteredMeals = menu.filter((meal) => {
+    if (meal.category == category) {
+      const mealCard = copyMealCard(meal);
+      mealContainer.appendChild(mealCard);
+    }
+  });
+}
+filterMeals(category);
+
+function copyMealCard(meal) {
+  const mealTemplate = document.importNode(template.content, true);
+  const card = mealTemplate.querySelector("#meal-card");
+
+  const photo = card.querySelector("#photo");
+  photo.setAttribute("src", meal.img);
+  const title = card.querySelector("#title");
+  title.innerText = meal.title;
+  const price = card.querySelector("#price");
+  price.innerText = meal.price;
+  const description = card.querySelector("#desc");
+  description.innerText = meal.desc;
+  return card;
+}
+
+btnAll.forEach(function (btn) {
+  btn.addEventListener("click", fillMealContainer);
+});
